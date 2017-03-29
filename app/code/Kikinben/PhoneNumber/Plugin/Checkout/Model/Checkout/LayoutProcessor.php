5599271c493608559ee5b15b5be72f6d897c6337
SathyashrayanN_Contus <sathyashrayan.n@contus.in>
2017-03-29 15:17:21 +0530

    Phone number strict for non-registered users

--- app/code/Kikinben/PhoneNumber/Plugin/Checkout/Model/Checkout/LayoutProcessor.php
+++ app/code/Kikinben/PhoneNumber/Plugin/Checkout/Model/Checkout/LayoutProcessor.php
@@ -3,10 +3,12 @@ namespace Kikinben\PhoneNumber\Plugin\Checkout\Model\Checkout;
 class LayoutProcessor
 {
     protected $_customerLogin;
+    protected $_customerDetails;
 
-    public function __construct(\Magento\Customer\Model\Session $customerLogin)
+    public function __construct(\Magento\Customer\Model\Session $customerLogin,\Magento\Customer\Model\Customer $customerDetails )
     {
         $this->_customerLogin = $customerLogin;
+        $this->_customerDetails = $customerDetails;
     }
     public function afterProcess(
         \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
@@ -14,24 +16,53 @@ class LayoutProcessor
     ) {
 	if(!$this->_customerLogin->isLoggedIn()){
 		$jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
-		['shippingAddress']['children']['shipping-address-fieldset']['children']['delivery_date'] = [
+		['shippingAddress']['children']['shipping-address-fieldset']['children']['telephone'] = [
 		    'component' => 'Magento_Ui/js/form/element/abstract',
 		    'config' => [
 		        'customScope' => 'shippingAddress',
 		        'template' => 'ui/form/field',
 		        'elementTmpl' => 'ui/form/element/input',
 		        'options' => [],
-		        'id' => 'delivery-date'
+		        'id' => 'telephone'
 		    ],
-		    'dataScope' => 'shippingAddress.delivery_date',
-		    'label' => 'Mobile Number',
+		    'dataScope' => 'shippingAddress.telephone',
+		    'label' => 'Telephone Number',
+		    'provider' => 'checkoutProvider',
+		    'visible' => true,
+            'validation' => [
+                'required-entry' => true,
+                'validate-phoneStrict'=>true
+            ],
+		    'sortOrder' => 250,
+		    'id' => 'telephone'
+		];
+        }
+    else if($this->_customerLogin->getId()){
+
+        $customerDetailsFetch = $this->_customerDetails->load($this->_customerLogin->getId());
+
+        	$jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
+		['shippingAddress']['children']['shipping-address-fieldset']['children']['telephone'] = [
+		    'component' => 'Magento_Ui/js/form/element/abstract',
+		    'config' => [
+		        'customScope' => 'shippingAddress',
+		        'template' => 'ui/form/field',
+		        'elementTmpl' => 'ui/form/element/hidden',
+		        'options' => [],
+		        'id' => 'telephone'
+		    ],
+		    'dataScope' => 'shippingAddress.telephone',
 		    'provider' => 'checkoutProvider',
 		    'visible' => true,
 		    'validation' => [],
 		    'sortOrder' => 250,
-		    'id' => 'delivery-date'
+            'id' => 'telephone',
+            'value'=>$customerDetailsFetch->getMobilenumber()
 		];
-        }
+
+
+
+    }
         return $jsLayout;
     }
 }
