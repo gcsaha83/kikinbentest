d53c774955d2b11d402989ab48d34fe2935ee67f
SathyashrayanNContus <sathyashrayan.n@contus.in>
2017-04-17 17:31:48 +0530

    otp and forget password

--- app/code/Kikinben/Otp/Controller/Index/Sendotp.php
+++ app/code/Kikinben/Otp/Controller/Index/Sendotp.php
@@ -4,6 +4,7 @@ namespace Kikinben\Otp\Controller\Index;
 
 use Magento\Framework\Controller\ResultFactory;
 
+
 class Sendotp extends \Magento\Framework\App\Action\Action
 {
     protected $_cacheTypeList;
@@ -12,6 +13,7 @@ class Sendotp extends \Magento\Framework\App\Action\Action
     protected $_resultPageFactory;
     protected $_storeManager;
     protected $_customerSession;
+    protected $_scopeConfig;
 
      public function __construct(
         \Magento\Framework\App\Action\Context $context,
@@ -20,7 +22,9 @@ class Sendotp extends \Magento\Framework\App\Action\Action
         \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Customer\Model\Session $customerSession
+        \Magento\Customer\Model\Session $customerSession,
+     	\Magento\Framework\App\Config\ScopeConfigInterface $scopeInterface
+     	
     ) {
         parent::__construct($context);
         $this->_cacheTypeList = $cacheTypeList;
@@ -29,9 +33,16 @@ class Sendotp extends \Magento\Framework\App\Action\Action
         $this->_resultPageFactory = $resultPageFactory;
         $this->_storeManager = $storeManager;
         $this->_customerSession = $customerSession;
+        $this->_scopeConfig = $scopeInterface;
+        
     }
     public function execute()
     {
+       $xmlStructure = array();	
+       $smsUserName = $this->_scopeConfig->getValue('twiliosetting/frontend_display/account_sid', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+       $smsUserPasswd = $this->_scopeConfig->getValue('twiliosetting/frontend_display/auth_token', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+       $smsSid = $this->_scopeConfig->getValue('twiliosetting/frontend_display/twilio_number', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+       //echo $smsUserName."==".$smsUserPasswd."==".$smsSid.'<br/>'; die;
        $resultRedirect = $this->resultFactory->create(
             ResultFactory::TYPE_REDIRECT);
         $params = $this->getRequest()->getParams();
@@ -104,13 +115,64 @@ if (count($modelOtp)) {
 
 try {
                         $to = str_replace('+', '', $fromNumber);                    
-                        $api = 'http://api.bulksms.top/2/?user=kikinben&pass=kikinben2016&to='.$to.'&sender=Kikinben&message='.urlencode($message);
-                        $sms = file_get_contents($api);
-                        $this->_customerSession
+                        //$api = 'http://api.bulksms.top/2/?user=kikinben&pass=kikinben2016&to='.$to.'&sender=Kikinben&message='.urlencode($message);
+                        //$sms = file_get_contents($api);
+                        $url="http://sms.sslwireless.com/pushapi/dynamic/server.php";
+                        $param="user=".$smsUserName."&pass=".$smsUserPasswd."&sms[0][0]=".$to."&sid=".$smsSid."&sms[0][1]=".urlencode($message);
+                        
+                        $crl = curl_init();
+                        curl_setopt($crl,CURLOPT_SSL_VERIFYPEER,FALSE);
+                        curl_setopt($crl,CURLOPT_SSL_VERIFYHOST,2);
+                        curl_setopt($crl,CURLOPT_URL,$url);
+                        curl_setopt($crl,CURLOPT_HEADER,0);
+                        curl_setopt($crl,CURLOPT_RETURNTRANSFER,1);
+                        curl_setopt($crl,CURLOPT_POST,1);
+                        curl_setopt($crl,CURLOPT_POSTFIELDS,$param);
+                        $response = curl_exec($crl);
+                        curl_close($crl);
+                        //echo $response;die;
+                        $xml = simplexml_load_string($response);
+                        $json = json_encode($xml);
+                        $smsArray = json_decode($json,TRUE);
+                        
+                        
+                        
+                        if($smsArray['PARAMETER']==="OK" && 
+                        		$smsArray['LOGIN'] ==="SUCESSFULL" && 
+                        		$smsArray['PUSHAPI'] === 'ACTIVE' &&
+                        		$smsArray['STAKEHOLDERID'] === 'OK' &&
+                        		$smsArray['PERMITTED'] === 'OK'
+                        		
+                        		)
+                        {
+                        	if($smsArray['SMSINFO']['REFERENCEID']){
+                        		
+                        		$this->_customerSession
+                        		->setMobileNumber($mobileNumber);
+                        		$resultRedirect->setUrl(
+                        				$this->_storeManager->getStore()
+                        				->getBaseUrl() . 'registration/index/index');
+                        		
+                        	}
+                        	else{
+                        		$this->messageManager->addError('We can\'t send the otp, something went wrong');
+                        		$resultRedirect->setUrl($this->_redirect->getRefererUrl());
+                        	}
+                        	
+                        }
+                        else{                        	
+                        	$this->messageManager->addError('We can\'t send the otp, something went wrong');
+                        	$resultRedirect->setUrl($this->_redirect->getRefererUrl());
+                        }
+                        	
+                        
+                        
+                        
+                       /* $this->_customerSession
                             ->setMobileNumber($mobileNumber);
                         $resultRedirect->setUrl(
                             $this->_storeManager->getStore()
-                                ->getBaseUrl() . 'registration/index/index');
+                                ->getBaseUrl() . 'registration/index/index');*/
 } catch (\Exception $e) {
                         $this->messageManager->addException($e,
                             __('We can\'t send the otp, 
@@ -131,6 +193,7 @@ try {
 
 
     }
+    
 
 
 
